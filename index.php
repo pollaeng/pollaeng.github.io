999ce7112ba0c59d0e1fdba843889e97755cb1c2
Polla Fattah <pollaeng@gmail.com>
2017-04-16 21:44:21 +0100

    testing php
    This is a test

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <!DOCTYPE html>
 <html>
 <body>
+<style>
+body{
+    background: gray;
+}
+</style>
 <h1>Hello World</h1>
+<?php
+    echo 'pho test';
+?>
 <p>I'm hosted with GitHub Pages.</p>
 </body>
 </html>
